e9e94c61508e2f33a6aa8e5867206d22688b7f37
Sergio Mendoza <smendoza787@gmail.com>
2016-09-17 23:51:44 -0700

    filled in the Scene class

---
+++ ex43.rb
@@ -0,0 +1,50 @@
+class Scene
+  def enter
+  end
+end
+
+
+class Engine
+
+  def initialize(scene_map)
+  end
+
+  def play()
+  end
+end
+
+class Death < Scene
+
+  def enter()
+  end
+end
+
+class CentralCorridor < Scene
+
+  def enter()
+  end
+end
+
+class LaserWeaponArmory < Scene
+
+  def enter()
+  end
+end
+
+class TheBridge < Scene
+
+  def enter()
+  end
+end
+
+class EscapePod < Scene
+
+  def enter()
+  end
+end
+
+
+class Map
+
+  def initi
+end
